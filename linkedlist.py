31f48a96a307e13364e7f63afdd6e94d2c63ef47
Chetan Dharmik <dharmikchetan1@gmail.com>
2021-08-10 16:22:52 +0530

    linkedlist added

---
+++ linkedlist.py
@@ -0,0 +1,158 @@
+# # A simple Python program for traversal of a linked list
+
+# # Node class
+# class Node:
+
+# 	# Function to initialise the node object
+# 	def __init__(self, data):
+# 		self.data = data # Assign data
+# 		self.next = None # Initialize next as null
+
+
+# # Linked List class contains a Node object
+# class LinkedList:
+
+# 	# Function to initialize head
+# 	def __init__(self):
+# 		self.head = None
+
+# 	# This function prints contents of linked list
+# 	# starting from head
+# 	def printList(self):
+# 		temp = self.head
+# 		while (temp):
+# 			print (temp.data)
+# 			temp = temp.next
+
+
+# # Code execution starts here
+# if __name__=='__main__':
+
+# 	# Start with the empty list
+# 	llist = LinkedList()
+
+# 	llist.head = Node(1)
+# 	second = Node(2)
+# 	third = Node(3)
+
+# 	llist.head.next = second; # Link first node with second
+# 	second.next = third; # Link second node with the third node
+
+# 	llist.printList()
+
+###################################################################################################################################################################################
+
+
+# #program to demonstrate deletion in singly linked list with class
+
+# Node class
+class Node:
+
+	# Constructor to initialize the node object
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+class LinkedList:
+
+	# Function to initialize head
+	def __init__(self):
+		self.head = None
+
+	# Function to insert a new node at the beginning
+	def push(self, new_data):
+		new_node = Node(new_data)
+		new_node.next = self.head
+		self.head = new_node
+
+	# Given a reference to the head of a list and a key,
+	# delete the first occurrence of key in linked list
+	def deleteNode(self, key):
+		
+		# Store head node
+		temp = self.head
+
+		# If head node itself holds the key to be deleted
+		if (temp is not None):
+			if (temp.data == key):
+				self.head = temp.next
+				temp = None
+				return
+
+		# Search for the key to be deleted, keep track of the
+		# previous node as we need to change 'prev.next'
+		while(temp is not None):
+			if temp.data == key:
+				break
+			prev = temp
+			temp = temp.next
+
+		# if key was not present in linked list
+		if(temp == None):
+			return
+
+		# Unlink the node from linked list
+		prev.next = temp.next
+
+		temp = None
+
+
+	# Utility function to print the linked LinkedList
+	def printList(self):
+		temp = self.head
+		while(temp):
+			print (" %d" %(temp.data)),
+			temp = temp.next
+
+
+# Driver program
+llist = LinkedList()
+llist.push(7)
+llist.push(1)
+llist.push(3)
+llist.push(2)
+
+print ("Created Linked List: ")
+llist.printList()
+llist.deleteNode(1)
+print ("\nLinked List after Deletion of 1:")
+llist.printList()
+
+###############################################################################################################################################################################
+
+#program to delte a node at given location
+# Node class
+class Node:
+
+	# Constructor to initialize the node object
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+class LinkedList:
+
+	# Function to initialize head
+	def __init__(self):
+		self.head = None
+
+	# Function to insert a new node at the beginning
+	def push(self, new_data):
+		new_node = Node(new_data)
+		new_node.next = self.head
+		self.head = new_node
+
+
+
+	def delete(self,location):
+		if self.head==None 
+			return
+        temp=self.head
+		
+
+
+
+
+
+
+
+
